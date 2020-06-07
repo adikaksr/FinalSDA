0a106ec5a9657133282d86a657e16673902abb26
adikaksr <64773004+adikaksr@users.noreply.github.com>
2020-06-07 14:27:25 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+            <meta charset="utf-8">
+                <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+                    <link href="assets\bootstrap\css\bootstrap.min.css" rel="stylesheet">
+                        <link href="assets\css\fontawesome-free\css\all.min.css" rel="stylesheet">
+                            <link href="assets\css\style.css" rel="stylesheet">
+                                <title>
+                                    Data Editor
+                                </title>
+                            </link>
+                        </link>
+                    </link>
+                </meta>
+            </meta>
+        </meta>
+    </head>
+    <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg"  style="background-color: lime">
+            <a style="color: black" class="navbar-brand font-weight-bold" href="index.php">
+                <i class="fas fa-edit">
+                </i>
+                DATA EDITOR
+            </a>
+            <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarSupportedContent" data-toggle="collapse" type="button">
+                <span class="navbar-toggler-icon">
+                </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a  style="color: black" class="nav-link" href="cari.php">
+                            PENCARIAN EDITOR
+                        </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a  style="color: black" class="nav-link" href="tampil.php">
+                            TAMPILKAN EDITOR
+                        </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a   style="color: black" class="nav-link" href="about.php">
+                            ABOUT ME
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- End Navbar -->
+        <div class="container mt-5">
+        <center>
+        
+            <p>
+               <b>Adika Nuraga Kanaka S.R.</b> <br>
+               Mahasiswa<br>
+               S1 Informatika Universitas Syiah Kuala
+               <br>
+            </p>
+            </center>
+        </div>
+        <script src="assets\bootstrap\js\jquery.min.js">
+        </script>
+        <script src="assets\bootstrap\js\bootstrap.min.js">
+        </script>
+    </body>
+</html>
